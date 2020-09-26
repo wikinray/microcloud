38bfb8583cf0c9a7404a3d3cd766706c708f09e5
weylei1990 <weiqinlei@jajaying.com>
2020-09-26 10:48:25 +0800

    支付服务提供者

---
+++ provider-payment8001/data.sql
@@ -0,0 +1,5 @@
+create table db2020.payment2(
+ `id` bigint(20) not null auto_increment comment 'ID',
+ `SERIAL` varchar(200) DEFAULT '',
+ primary KEY (`id`)
+)ENGINE=InnoDB AUTO_INCREMENT=1 default CHARSET=utf8;
