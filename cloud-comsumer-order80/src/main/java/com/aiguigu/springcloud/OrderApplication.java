8fca96465dcce545e65f40a03bccea59b7e24c12
weylei1990 <weiqinlei@jajaying.com>
2020-10-07 14:50:00 +0800

    ribbon负载均衡逻辑替换为随机 1、新建配置类，不能在@ComponentScan包及以下包内 2、启动配置增加@RibbonClient指定 调用服务所使用得替换配置（随机规则）

--- cloud-comsumer-order80/src/main/java/com/aiguigu/springcloud/OrderApplication.java
+++ cloud-comsumer-order80/src/main/java/com/aiguigu/springcloud/OrderApplication.java
@@ -1,8 +1,11 @@
 package com.aiguigu.springcloud;
 
+import com.aiguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 @SpringBootApplication
 public class OrderApplication {
     public static void main(String[] args) {
